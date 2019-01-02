93dcd0956fa95eb1b8b1de3fc9684af5bab4e567
Jakub Turkiewicz <turkiewicz@transporeon.com>
2019-01-02 13:50:41 +0100

    test

--- spec.js
+++ spec.js
@@ -2,16 +2,16 @@ let EC = protractor.ExpectedConditions;
 browser.ignoreSynchronization = true;
 let fiveSeconds = 5000;
 
-describe('pajacyk clicker', function() {	
-	let greenButton = $('.paj-click');
-	let activeThanks = $('.dziekujemy.active');
+describe('pajacyk clicker', function() {
+    let greenButton = $('.paj-click');
+    let activeThanks = $('.dziekujemy.active');
 
-  	it('should be able to click on belly', function() {
-		browser.get('https://www.pajacyk.pl');
-		greenButton.click();
-		
-		browser.wait(EC.visibilityOf(activeThanks), fiveSeconds);
-		browser.wait(EC.invisibilityOf(activeThanks), fiveSeconds);
-  	});
+    it('should be able to click on belly', function() {
+        browser.get('https://www.pajacyk.pl');
+        greenButton.click();
+
+        browser.wait(EC.visibilityOf(activeThanks), fiveSeconds);
+        browser.wait(EC.invisibilityOf(activeThanks), fiveSeconds);
+    });
 
 });
