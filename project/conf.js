fec1e132c9db94e21549c89eed5920d7949a11bf
Jakub Turkiewicz <turkiewicz@transporeon.com>
2019-01-07 12:24:05 +0100

    added multi page form classes with test suite

--- project/conf.js
+++ project/conf.js
@@ -1,11 +1,11 @@
 exports.config = {
 	framework: 'jasmine',
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['pajacyk.spec.js', 'okruszek.spec.js'],
+	specs: ['pajacyk.spec.js', 'okruszek.spec.js', 'way2automation.spec.js'],
 	capabilities: {
 		browserName: 'chrome',
 		shardTestFiles: true,
-		maxInstances: 2,
+		maxInstances: 5,
 		chromeOptions: {
 			args: ['--disable-infobars',
 				   '--disable-gpu',
