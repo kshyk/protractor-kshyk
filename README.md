d6eeb02c9f7d00e1090dad45e6b5719f3c27ef9d
kshyk <jj.turkiewicz@gmail.com>
2020-04-26 21:34:06 +0200

    typo + formatting

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/b0944dd3052f49f88f9214db097eb800)](https://app.codacy.com/manual/kshyk/protractor-kshyk?utm_source=github.com&utm_medium=referral&utm_content=kshyk/protractor-kshyk&utm_campaign=Badge_Grade_Dashboard)
 
-repository made for protractor (webdriverjs) basics
+repository made for a protractor (webdriverjs) basics
--- project/conf.js
+++ project/conf.js
@@ -1,13 +1,13 @@
 exports.config = {
-	framework: "jasmine",
-	seleniumAddress: "http://localhost:4444/wd/hub",
-	specs: ["pajacyk.spec.js", "okruszek.spec.js", "way2automation.spec.js"],
-	capabilities: {
-		browserName: "chrome",
-		shardTestFiles: true,
-		maxInstances: 5,
-		chromeOptions: {
-			args: ["--disable-infobars", "--disable-gpu", "--disable-dev-shm-usage", "--start-maximized", "--no-sandbox"]
-		}
-	}
-}
+    framework: "jasmine",
+    seleniumAddress: "http://localhost:4444/wd/hub",
+    specs: ["pajacyk.spec.js", "okruszek.spec.js", "way2automation.spec.js"],
+    capabilities: {
+        browserName: "chrome",
+        shardTestFiles: true,
+        maxInstances: 5,
+        chromeOptions: {
+            args: ["--disable-infobars", "--disable-gpu", "--disable-dev-shm-usage", "--start-maximized", "--no-sandbox"]
+        }
+    }
+};
